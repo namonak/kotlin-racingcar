fb1020489d3abf142b93f7f86630eb5231f46647
namonak <namonak@gmail.com>
2023-01-08 22:24:27 +0900

    refactor(domain): move the constant values into the companion object

--- src/main/kotlin/racingcar/domain/car/Cars.kt
+++ src/main/kotlin/racingcar/domain/car/Cars.kt
@@ -2,8 +2,6 @@ package racingcar.domain.car
 
 import racingcar.domain.car.action.MovingStrategy
 
-private const val MINIMUM_NUMBER_OF_RACERS = 2
-
 data class Cars(val cars: List<Car>) {
     init {
         require(cars.size >= MINIMUM_NUMBER_OF_RACERS) { "자동차는 2대 이상이어야 합니다." }
@@ -18,5 +16,9 @@ data class Cars(val cars: List<Car>) {
     fun findTheWinner(): List<String> {
         val maxPosition = cars.maxOf { it.position.value }
         return cars.filter { it.position.value == maxPosition }.map { it.name.value }
+    }
+
+    companion object {
+        private const val MINIMUM_NUMBER_OF_RACERS = 2
     }
 }
--- src/main/kotlin/racingcar/domain/input/PlayTime.kt
+++ src/main/kotlin/racingcar/domain/input/PlayTime.kt
@@ -1,9 +1,11 @@
 package racingcar.domain.input
 
-private const val MINIMUM_PLAY_TIME = 0
-
 data class PlayTime(val value: Int) {
     init {
         require(value > MINIMUM_PLAY_TIME) { "시도 횟수는 0보다 커야 합니다." }
+    }
+
+    companion object {
+        private const val MINIMUM_PLAY_TIME = 0
     }
 }
